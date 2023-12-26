d94c21e3e4c566d50f3f0285a574cab68ffd7f19
AlicjaView <139495112+AlicjaView@users.noreply.github.com>
2023-12-26 18:19:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,22 @@
 # config.py
 
 class Config:
-    # Ustawienia dotyczące bazy danych
-    # Uwaga: Poniższe wartości to przykładowe. Dostosuj je do swoich własnych danych.
-    DB_HOST = "localhost"  # Adres hosta bazy danych
-    DB_PORT = 5432  # Port bazy danych
-    DB_USERNAME = "your_db_user"  # Nazwa użytkownika bazy danych
-    DB_PASSWORD = "your_db_password"  # Hasło bazy danych
-    DB_NAME = "your_db_name"  # Nazwa bazy danych
+    # Database settings
+    # Note: The following values are examples. Customize them with your own data.
+    DB_HOST = "localhost"  # Database host address
+    DB_PORT = 5432  # Database port
+    DB_USERNAME = "your_db_user"  # Database username
+    DB_PASSWORD = "your_db_password"  # Database password
+    DB_NAME = "your_db_name"  # Database name
 
-    # Ustawienia dla powiadomień email
-    # Uwaga: Wprowadź swoje dane, aby umożliwić powiadomienia email.
-    EMAIL_ADDRESS = "your_email@example.com"  # Adres e-mail
-    EMAIL_PASSWORD = "your_email_password"  # Hasło do konta e-mail
-    ADMIN_EMAIL = "admin@example.com"  # Adres e-mail administratora
+    # Email notification settings
+    # Note: Enter your details to enable email notifications.
+    EMAIL_ADDRESS = "your_email@example.com"  # Email address
+    EMAIL_PASSWORD = "your_email_password"  # Email account password
+    ADMIN_EMAIL = "admin@example.com"  # Administrator's email address
 
-    # Inne ustawienia
-    # Uwaga: Poniższe ustawienia to przykładowe. Dostosuj je do swoich potrzeb.
-    LOG_FILE_PATH = "security_analysis.log"  # Ścieżka do pliku dziennika logów
-    DEBUG_MODE = False  # Tryb debugowania
+    # Other settings
+    # Note: The following settings are examples. Customize them according to your needs.
+    LOG_FILE_PATH = "security_analysis.log"  # Path to the log file
+    DEBUG_MODE = False  # Debug mode
+
